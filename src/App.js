39ef775098d4126fd8b12c5a564017555e66ef2c
Ido Shenbach <idoshen1@gmail.com>
2025-02-03 14:11:51 +0200

    Added SpeedInsight component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Wordle from './projects/Wordle';
 import HTTPServer from './projects/HTTP-Server';
 import Contact from './pages/Contact';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   const [isLight, setIsLight] = useState(true);
@@ -106,6 +107,7 @@ function App() {
           <HTTPServer />
           <Contact />
         </main>
+        <SpeedInsights />
         <Analytics />
       </div>
   );
