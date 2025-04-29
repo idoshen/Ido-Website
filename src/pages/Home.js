c5fa7dcb33d45778ce49e4a49e770cc3923ade8f
Ido Shenbach <idoshen1@gmail.com>
2025-04-29 19:12:13 +0300

    Update CV content and improve link behavior in Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ function Home({isDarkMode}) {
             tackling complex algorithms, or contributing to impactful projects in tech.
           </p>
           <div className="home-buttons">
-            <a href="./Ido Shenbach CV.pdf" className={`home-cv-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`} download="Ido_Shenbach_CV.pdf">
+            <a href="./Ido Shenbach CV.pdf" className={`home-cv-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`} target="_blank" rel="noopener noreferrer" download="Ido_Shenbach_CV.pdf">
               <strong>Download CV</strong>
             </a>
             <a className={`home-contact-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`} href='#contact'>
